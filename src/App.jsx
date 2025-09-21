093932a2905338adec443e092d912367fb92d44c
sivalakshmik <soniakv.2822@gmail.com>
2025-09-21 09:01:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/register" element={<Register />} />
         <Route path="/" element={<Login />} />
         <Route path="/forgot" element={<ForgotPassword />} />
         <Route path="/reset/:token" element={<ResetPassword />} />
