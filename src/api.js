8b9e08e1555a58c37bc9b4b766ec458e45ad35cd
sivalakshmik <soniakv.2822@gmail.com>
2025-09-21 08:15:15 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Backend API URL
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL = "https://password-reset-1-a8ig.onrender.com";
 
 // Named exports for all API calls
 export const registerUser = (data) => axios.post(`${API_URL}/register`, data);
@@ -11,3 +11,4 @@ export const loginUser = (data) => axios.post(`${API_URL}/login`, data);
 export const forgotPassword = (data) => axios.post(`${API_URL}/forgot-password`, data);
 
 export const resetPassword = (token, data) => axios.post(`${API_URL}/reset-password/${token}`, data);
+
